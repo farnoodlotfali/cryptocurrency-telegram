ae96a1fa31274f3fc907e4efa79bce23b05f0a9e
farnoodlotfali <farnoodlotfali@gmail.com>
2024-10-28 21:55:07 +0330

    add const values

--- oogway/Shared/Constant.py
+++ oogway/Shared/Constant.py
@@ -8,3 +8,29 @@ class PostStatusValues(Enum):
     CANCELED = 'CANCELED'
     PENDING = 'PENDING'
     ERROR = 'ERROR'
+
+class PostStatusTypeValues(Enum):
+    FAILED_WITH_PROFIT = [PostStatusValues.FAILED_WITH_PROFIT.value,1,2,3,4,5,6,7,8]
+    SUCCESS = [PostStatusValues.SUCCESS.value,1,2,3,4,5,6,7,8]
+    FULLTARGET = [PostStatusValues.FULLTARGET.value,1000]
+    FAILED = [PostStatusValues.FAILED.value,-1]
+    CANCELED = [PostStatusValues.CANCELED.value,0]
+    PENDING = [PostStatusValues.PENDING.value,0]
+    ERROR = [PostStatusValues.ERROR.value,0]
+
+
+class MarketValues(Enum):
+    SPOT = 'SPOT'
+    FUTURES = 'FUTURES'
+
+
+class PositionSideValues(Enum):
+    LONG = 'LONG'
+    SHORT = 'SHORT'
+    BUY = 'BUY'
+
+
+class MarginModeValues(Enum):
+    ISOLATED = 'ISOLATED'
+    CROSS = 'CROSS'
+
