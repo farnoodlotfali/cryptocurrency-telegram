39e01f677759fa6efe6c931992efc6457d5c23ea
farnoodlotfali <farnoodlotfali@gmail.com>
2024-11-02 16:09:45 +0330

    remove unneeded files

--- oogway/Shared/loadOHLC_FromJson.py
+++
@@ -1,8 +0,0 @@
-from Shared.helpers import load_json
-
-def loadOHLC_FromJson(symbolName, path):
-   try:
-        data = load_json(f"{path}/{symbolName}.json")
-   except FileNotFoundError:
-        data = []  
-   return data
