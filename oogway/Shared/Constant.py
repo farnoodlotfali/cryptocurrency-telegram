c36cd74f657b0c8a95edabeb9d84f7a3f8e27a19
farnoodlotfali <farnoodlotfali@gmail.com>
2024-11-02 16:15:19 +0330

    add new status

--- oogway/Shared/Constant.py
+++ oogway/Shared/Constant.py
@@ -8,6 +8,7 @@ class PostStatusValues(Enum):
     CANCELED = 'CANCELED'
     PENDING = 'PENDING'
     ERROR = 'ERROR'
+    WAIT_MANY_DAYS = 'WAIT MANY DAYS'
 
 class PostStatusTypeValues(Enum):
     FAILED_WITH_PROFIT = [PostStatusValues.FAILED_WITH_PROFIT.value,1,2,3,4,5,6,7,8]
@@ -17,6 +18,7 @@ class PostStatusTypeValues(Enum):
     CANCELED = [PostStatusValues.CANCELED.value,0]
     PENDING = [PostStatusValues.PENDING.value,0]
     ERROR = [PostStatusValues.ERROR.value,0]
+    WAIT_MANY_DAYS = [PostStatusValues.WAIT_MANY_DAYS.value,0]
 
 
 class MarketValues(Enum):
@@ -34,3 +36,9 @@ class MarginModeValues(Enum):
     ISOLATED = 'ISOLATED'
     CROSS = 'CROSS'
 
+
+# # for webPanel you can check SettingConfig model in PostAnalyzer\models.py
+# SettingConfigsVariables = {
+#     # a days that we must wait for finding status of order(signal) in statistics method (such as statistic_PredictParts )
+#     'max_day_wait': 10,
+# }
