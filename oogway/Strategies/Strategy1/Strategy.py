a4d35275ba5b5093ca22fb95b9faffc63e367ad1
farnoodlotfali <farnoodlotfali@gmail.com>
2024-11-13 22:49:00 +0330

    add desc for strategy 1

--- oogway/Strategies/Strategy1/Strategy.py
+++ oogway/Strategies/Strategy1/Strategy.py
@@ -1,158 +1,151 @@
 
-from Shared.helpers import print_colored, compare_two_numbers
 from Shared.Constant import PostStatusValues
-from typing import Optional
+from PostAnalyzer.models import (
+    Predict,
+    TakeProfitTarget,
+)
+from Strategies.AbsStrategy import AbsStrategy
+from Shared.Constant import PostStatusValues 
 from asgiref.sync import sync_to_async
 from PostAnalyzer.models import (
     Predict,
     TakeProfitTarget,
-    EntryTarget
+    StopLoss,
 )
-from django.forms.models import model_to_dict
 
 # ****************************************************************************************************************************
 
-def calProfit(positionSize: float, profit: float)-> float:
-    return positionSize*(profit/100)
 
-def historyController(history: list, predict: Predict, curr_money:float, profit:float, take_profit_target: Optional[TakeProfitTarget]= None, entry_target: Optional[EntryTarget]= None):
-    model = model_to_dict(predict)
-    model['symbol'] = predict.symbol.name
-    model['market'] = predict.market.name
-    model['position'] = predict.position.name
+class Strategy1(AbsStrategy):
+    strategy_name = 'strategy1'
 
-    model['current_money'] = curr_money
-    model['profit'] = profit
+    async def backtest_with_money_strategy_1(self, predicts:list[Predict], close_tp:int=1, showPrint:bool= False, positionSize:float= 100):
 
-    model['tp_value'] = take_profit_target.value if take_profit_target else None
-    model['tp_index'] = take_profit_target.index if take_profit_target else None
-    model['tp_date'] = take_profit_target.date if take_profit_target else None 
-    
-    model['entry_value'] = entry_target.value if entry_target else None
-    model['entry_index'] = entry_target.index if entry_target else None
-    model['entry_date'] = entry_target.date if entry_target else None
-    history.append(model)
+        for i, pr in enumerate(predicts):
+            statusName = pr.status.name
 
-    return history
+            if statusName in [PostStatusValues.CANCELED.value, PostStatusValues.ERROR.value, PostStatusValues.WAIT_MANY_DAYS.value]:
+                continue
 
-#strategy_1
-async def backtest_with_money_strategy_1(predicts: list[Predict], my_money: float, close_tp: int=0, showPrint: bool=False, positionSize: float= 100):
-   
-    initial_my_money = my_money
-    total_opening_orders = 0
-
-
-    # profit
-    profit_total = 0
-    profit_count = 0
-
-    # loss
-    loss_total = 0
-    loss_count = 0
-
-
-    # pending
-    pending_total = 0
-    pending_count = 0
-
-    # canceled
-    canceled_count = 0
-
-    history = []
-
-    for i, pr in enumerate(predicts):
-
-        if pr.status.name == PostStatusValues.CANCELED.value:
-            canceled_count += 1
-            continue
-        
-
-        entries = await sync_to_async(
-            lambda: list(EntryTarget.objects.filter(post=pr.post, active=True).order_by('index'))
-        )()
-
-        if pr.status.name == PostStatusValues.PENDING.value:
-            pending_count += 1
             
-            if len(entries) >= 1:
-                total_opening_orders += 1
-                my_money -= positionSize
-                pending_total += positionSize
-                history = historyController(history=history, predict=pr, curr_money= my_money)
-            continue
-        
+            start_timestamp = int(pr.date.timestamp() * 1000)
 
-        first_entry = entries[0]
+            # current money
+            self.current_money += self.checkFreeMoneyManagement(start_timestamp)
 
-        my_money -= positionSize
-        total_opening_orders += 1
+            # blocked money
+            self.total_pending_money = self.checkFBlockedMoneyManagement()
+
+            if showPrint:
+                print(f'{pr.symbol.name}, current_money: {round(self.current_money,2)}, blocked_money: {round(self.total_pending_money,2)}')
 
 
-        if pr.status.name == PostStatusValues.FAILED.value:
-            loss_count += 1
+            # missed order :(
+            if self.current_money < positionSize:
+                if showPrint:
+                    print(pr.symbol.name, 'missed\n')
 
-            loss = calProfit(positionSize, pr.profit) # is negative profit
-            my_money += positionSize + loss
-            loss_total += loss
+                self.addMissOrder(order=pr)
+                
+                continue 
 
-            history = historyController(history=history, predict=pr, curr_money= my_money, profit=loss, entry_target=first_entry)
-            continue
-
-
-        take_profits = await sync_to_async(
-            lambda: list(TakeProfitTarget.objects.filter(post=pr.post, active=True).order_by('index'))
-        )()
+            profit = 0
 
 
-        if pr.status.name == PostStatusValues.SUCCESS.value or pr.status.name == PostStatusValues.FULLTARGET.value:
-            profit_count += 1
+            # get take-profits 
+            take_profit = await sync_to_async(
+                lambda: list(TakeProfitTarget.objects.filter(predict=pr).order_by('index'))
+            )()
 
-            tp = take_profits[close_tp if len(take_profits)-1 >= close_tp else -1]
+            active_take_profit = await sync_to_async(
+                lambda: list(TakeProfitTarget.objects.filter(predict=pr, active=True).order_by('index'))
+            )()
 
-            profit = calProfit(positionSize, tp.profit) # is positive profit
+            # get stoploss 
+            stoploss = await sync_to_async(StopLoss.objects.get)(predict=pr)
+
+
+
+            # add positionSize to pending money
+            self.total_pending_money += positionSize
+            self.pending_count += 1
+            # it reduces current_money for creating order
+            self.current_money -= positionSize
+
+            if showPrint:
+                print(f'money_taken: {positionSize}, current_money: {round(self.current_money, 2)}')
+
+
+            # the order has been sent and submitted, so added to active orders
+            self.addActiveOrder(order=pr, positionSize=positionSize)
+
+            # the order`s status is pending, so added to pending orders
+            self.addPendingOrder(pr)
+
+            is_hit = False
             
-            my_money += positionSize + profit
-            profit_total += profit 
+            tp_len = len(take_profit)
+            active_tp_len = len(active_take_profit)
+            is_fulltarget = active_tp_len >= close_tp
 
-            history = historyController(history=history, predict=pr, curr_money= my_money, profit=profit, entry_target=first_entry, take_profit_target=tp)
+            if close_tp > tp_len:
+                is_fulltarget = tp_len == active_tp_len
 
-            continue
+            # if (active_tp_len < close_tp) and (tp_len != active_tp_len):
+            #     continue
 
-        if pr.status.name == PostStatusValues.FAILED_WITH_PROFIT.value:
-            if len(take_profits)-1 >= close_tp:
-                profit_count += 1
+
+            if is_fulltarget or (statusName == PostStatusValues.SUCCESS.value):
+                is_hit = True
+
+                profit = active_take_profit[active_tp_len-1].profit/100
+                date = int(active_take_profit[active_tp_len-1].date.timestamp()*1000)
+                self.updateMoneyManagement(id=pr.id, end_date=date, money_back=positionSize*(1+profit))
+                self.addProfitOrder(order=pr, profit=profit, status_date=date, type= active_tp_len if statusName == PostStatusValues.SUCCESS.value else 1000)
+
+            elif statusName in [PostStatusValues.FAILED_WITH_PROFIT.value, PostStatusValues.FAILED.value]:
+                is_hit = True
+                profit = pr.profit/100
+                date = int(stoploss.date.timestamp()*1000)
+                self.updateMoneyManagement(id=pr.id, end_date=date, money_back=positionSize*(1+profit))
+                self.addLossOrder(order=pr, profit=profit, status_date=date)
+
+            # self.orderDetailController(entry_targets=entry_reached, predict=pr, stopLoss=stop_loss, take_profit_targets=tps, stopLoss_time=stop_loss_reached)
+
+            if profit < 0:
+                self.total_loss += profit
+                self.loss_count += 1
+                self.pending_count -= 1
+            elif profit > 0:
+                self.total_profit += profit
+                self.profit_count += 1
+                self.pending_count -= 1
+
+            if showPrint:
+
+                if is_hit:
+                    print(f'profit: {round(profit,2)} %, money back: {round(positionSize*(1+profit), 2)}')
+                    print(f'current_money: {round(self.current_money+(positionSize*(1+profit)),2)}')
+                else:
+                    print(f'take-profit or stoploss or entry target has not been reach completely, so pending')
                 
-                tp = take_profits[close_tp]
 
-                profit = calProfit(positionSize, tp.profit) # is positive profit
-                
-                my_money += positionSize + profit
-                profit_total += profit
+                print('\n\n')
 
-                history = historyController(history=history, predict=pr, curr_money= my_money, profit=profit, entry_target=first_entry, take_profit_target=tp)
-
-            else:
-                loss_count += 1
-                
-                loss = calProfit(positionSize, pr.profit) # is negative profit
-                
-                my_money += positionSize + loss
-                loss_total += loss
-
-                history = historyController(history=history, predict=pr, curr_money= my_money, profit=loss, entry_target=first_entry)
+        self.giveBackAllActiveMoney()
 
 
-    if showPrint:
-        print_colored(f"initial money: {initial_my_money}", "gold")
-        print_colored(f"total_opening_orders: {total_opening_orders}", "#1da44f")
-        print_colored(f"loss_total: {loss_total}", "pink")
-        print_colored(f"profit_total: {profit_total}", "green")
-        print_colored(f"gross: {profit_total+loss_total}", "deeppink")
-        print_colored(f"pending_total: {pending_total}", "grey")
-        print_colored(f"my free money: {my_money}", "orange")
-        print_colored(f"my total money: {my_money+pending_total}", "#d16984")
-        print_colored(f"profit_count: {profit_count}, loss_count: {loss_count}, pending_count: {pending_count}", "#a1309d")
+            
 
-    return history
+
+
+
+            
+
+
+
+
+
+
 
 
---
+++ oogway/Strategies/Strategy1/desc.md
@@ -0,0 +1,27 @@
+# Strategy 1
+
+This strategy is a real-time strategy.
+This strategy actually does the same strategy as the channel itself.
+### Initial Strategy
+---
+money = is a mount of money that we want to use it in this strategy. Required
+
+timeZone =  is a time zone. default = UTC
+
+### Running Parameters
+---
+
+predicts = a list of predict. Required
+
+close_tp =  is a take-profit index that the order must be close. default = 1
+
+showPrint = is a flag that allow to print a details. default = False
+
+positionSize = is a mount of money for opening a order. default = 100
+
+## How it works
+This strategy just act like channels strategy. So we Evaluate channels strategy.
+
+
+
+
