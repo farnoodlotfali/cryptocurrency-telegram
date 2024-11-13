8cceacda929d7d351592772d8bbc8908c119e8e3
farnoodlotfali <farnoodlotfali@gmail.com>
2024-11-13 22:44:08 +0330

    add PredictNoModel to constant

--- oogway/Shared/types.py
+++ oogway/Shared/types.py
@@ -8,3 +8,18 @@ class Stat(TypedDict):
     entry_reached: List[Any]       
     stop_loss_reached: Optional[Any] 
     break_reason: Optional[Any]  
+
+
+class PredictNoModel(TypedDict):
+    id: int               
+    tps: List[float]                  
+    entries: List[float]       
+    stop_loss: float
+    symbol: str
+    market: str
+    position: str
+    leverage: int
+    margin_mode: str
+    date: int
+    
+ 
